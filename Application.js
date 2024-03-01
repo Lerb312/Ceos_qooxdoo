a5b36a44b06be55b824a4f34b0eb5391fb4168c0
Lerb312 <138533020+Lerb312@users.noreply.github.com>
2024-03-01 14:15:08 -0400

    Modificaciones en el toolbar de la ventanasuperior

--- Application.js
+++ Application.js
@@ -31,18 +31,17 @@ qx.Class.define("myejemplo.Application",
     
     main()
     {
-      // Call super class
+    
       super.main();
-      qx.log.Logger.info(this, "antes de todo");
+    
       //importando el modulo echarts
 	var myEcharts = require("echarts");//fino
 	
-      // Enable logging in debug variant
+      
       if (qx.core.Environment.get("qx.debug"))
       {
-        // support native logging capabilities, e.g. Firebug for Firefox
+       
         qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
         qx.log.appender.Console;
       }
       
@@ -51,7 +50,9 @@ qx.Class.define("myejemplo.Application",
 	var win = new qx.ui.window.Window("Ventana");
    
 	win.setWidth(1200);
+	win.setMaxWidth(2000);
 	win.setHeight(800);
+	win.setMaxHeight(1500);
 	win.setShowMinimize(false);
 	
 	win.setCenterOnAppear(true);
@@ -62,38 +63,46 @@ qx.Class.define("myejemplo.Application",
 	
 	//Contenedores
 	var com1 = new qx.ui.container.Composite();
-	//com1.setBackgroundColor("blue");
-	com1.setLayout( new qx.ui.layout.Canvas());
+	com1.setBackgroundColor("green");
+	var grid = new  qx.ui.layout.Grid(10, 12);
+	//Ajuste del tamaño de la celda del layout grid
+	grid.setRowHeight(0, 34);
+	
+	
+	com1.setLayout(grid);
+	
 	
 	 var com2 = new qx.ui.container.Composite();
 	com2.setBackgroundColor("red");
-	com2.setLayout( new qx.ui.layout.Canvas());
+	com2.setLayout( new qx.ui.layout.Grid(2, 1));
 	qx.log.Logger.info(this, "2");
 	
-	var com3 = new qx.ui.container.Composite();
-	com3.setBackgroundColor("green");
+	
+	 var com3 = new qx.ui.container.Composite();
 	com3.setLayout( new qx.ui.layout.Canvas());
-	com3.setMargin(10);
+	 
+	com3.setWidth(com1.getWidth());
 	//Lienzo donde se dibuja la figura
+	
+	
 	var canvas1 = new qx.ui.embed.Canvas().set({
-        canvasWidth: 200,
-        canvasHeight: 200,
+        canvasWidth: 500,
+        canvasHeight: 500,
         syncDimension: false,
       });
-	
+      
 	  //4)
      //EVENTO QUE REDIBUJA EL LIENZO
      canvas1.addListener("redraw", function(e)
       {
       
        var chartDom = com3.getContentElement().getDomElement();
-      qx.log.Logger.info(this, chartDom);
-        qx.log.Logger.info(this, "estoy aqui");
+       qx.log.Logger.info(this, chartDom);
 
-      	var myChart = myEcharts.init(chartDom, 'dark');//fino
+      var myChart = myEcharts.init(chartDom, 'dark');//fino
         var option;
 
-	qx.log.Logger.info(this, "ahora aqui");
+	
 	
         const data2 = [];
         for (let i = 0; i <= 360; i++) {
@@ -128,7 +137,7 @@ option = {
      //\4)
      
 	
-	//toolbarsn()
+	//toolbars
 	var toolbar = new qx.ui.toolbar.ToolBar();
 	var toolbar2 = new qx.ui.toolbar.ToolBar();
 	
@@ -137,13 +146,12 @@ option = {
 	
 	//Panel horizontal
 	var splitPane = new qx.ui.splitpane.Pane();
-	 splitPane.setOrientation("vertical");
-	qx.log.Logger.info(splitPane.getOrientation());
+	splitPane.setOrientation("vertical");
+	splitPane.setWidth(2000);
+	splitPane.setHeight(1500);
 	
-	
-	
-	
-	
+
+
 	//Botones del toolbar
 	var btnNuevo = new qx.ui.toolbar.MenuButton("Nuevo");
 	var btnAbrir = new qx.ui.toolbar.MenuButton("Abrir");
@@ -154,6 +162,7 @@ option = {
 	var btnGuardar2 = new qx.ui.toolbar.MenuButton("Guardar");
 	
 	
+	//Menus
 	var menu = new qx.ui.menu.Menu();
 	var opc1 = new qx.ui.menu.Button("Archivo .doc");
 	var opc2 = new qx.ui.menu.Button("Archivo .xls");
@@ -162,43 +171,59 @@ option = {
 	var opc11 = new qx.ui.menu.Button("Archivo .doc");
 	var opc22 = new qx.ui.menu.Button("Archivo .xls");
 	
+	//Se añaden opciones al menu1
 	qx.log.Logger.info(this, "3");
 	menu.add(opc1);
 	menu.add(new qx.ui.menu.Separator());
 	menu.add(opc2);
 	
+	//Se añaden opciones al menu2
 	menu2.add(opc11);
 	menu2.add(new qx.ui.menu.Separator());
 	menu2.add(opc22);
 	
+	//El menuboton añade su menu1
 	btnNuevo.setMenu(menu);
+	btnNuevo.setWidth(80);
 	
+	//El menuboton2 añade su menu2
 	btnNuevo2.setMenu(menu2);
 	qx.log.Logger.info(this, "4");
 	
 	
-	//toolbar.add(boton);
+	//toolbar de la parte superior añade elementos dentro de si
 	toolbar.add(btnNuevo);
 	toolbar.add(btnAbrir);
 	toolbar.add(btnGuardar);
 	
-	
+	//toolbar de la parte inferior  añade elementos dentro de si
 	toolbar2.add(btnNuevo2);
 	toolbar2.add(btnAbrir2);
 	toolbar2.add(btnGuardar2);
+	
 	qx.log.Logger.info(this, "5");
 	
 	
-	com1.add(com3, {edge: 0});
-	com1.add(toolbar, {top: 0});
-	com2.add(toolbar2, {top: 0});
+
+	//contenedor 1 añade su toolbar 
+	com1.add(toolbar, {row: 0, column: 0});
+	//contenedor 1 añade otro contenedor para mostrar el grafico
+	com1.add(com3, {row: 1, column: 0, rowSpan: 30, colSpan: 150});
+	//contenedor 2 añade su toolbar
+	com2.add(toolbar2, {row: 1, column: 1});
 	
+	//panel dividido añade ambos contenedores
+	splitPane.add(com1, 1);
+	splitPane.add(com2, 2);
 	
-	splitPane.add(com1);
-	splitPane.add(com2);
-	
-	
+	qx.log.Logger.info(splitPane.getWidth());
+	com1.setWidth(splitPane.getWidth());
+	qx.log.Logger.info(com1.getWidth());
 	win.getChildrenContainer().add(splitPane, {edge: 0});
+	
+	canvas1.addListener("resize", function(){
+	canvas1.setCanvasWidth(com1.getWidth());
+	} );
 	
 	this.getRoot().add(win, {edge: 0});
 	win.open();
