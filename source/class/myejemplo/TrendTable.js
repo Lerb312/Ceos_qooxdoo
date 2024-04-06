017ae4cb10577824b734d6ac49b7b55b81aa0145
Lerb312 <138533020+Lerb312@users.noreply.github.com>
2024-04-06 19:46:36 -0400

    ContextMenu update

--- source/class/myejemplo/TrendTable.js
+++ source/class/myejemplo/TrendTable.js
@@ -3,7 +3,7 @@
 qx.Class.define("myejemplo.TrendTable", {
   extend: qx.ui.table.Table,
 
-  construct: function (columnas) {
+  construct: function (columnas, id) {
     this.base(arguments);
     // table model config
     let tableModel = new qx.ui.table.model.Simple();
@@ -12,7 +12,13 @@ qx.Class.define("myejemplo.TrendTable", {
    
     
     this.setTableModel(tableModel);
-    this.mouseEventTable();
+    
+    if(id == "Trace"){
+    	this.mouseEventTable2();
+    }else if(id == "ValueAxes"){
+    	 this.mouseEventTable();
+    }
+   
   },
   
   members:{
@@ -27,7 +33,41 @@ qx.Class.define("myejemplo.TrendTable", {
   		
   		let btn1 = new qx.ui.menu.Button("Add Axis");
 		
+			btn1.addListener("execute", function(){
+				//logica para agregar registros a la tabla
+			
+			});
 		menu.add(btn1);
+		
+		menu.openAtPointer(e); 
+  		});
+  	
+  	
+  	},
+  	
+  	mouseEventTable2:function(){
+  	
+  		this.addListener("contextmenu", function(e){
+  		
+  		 
+      		let menu = new qx.ui.menu.Menu();
+  		menu.setOpener(this);
+  		
+  		let btn1 = new qx.ui.menu.Button("Add PV");
+		let btn2 = new qx.ui.menu.Button("Add Formula");
+			
+			btn1.addListener("execute", function(){
+				//logica 
+			
+			});
+			
+			btn2.addListener("execute", function(){
+				//logica 
+			
+			});
+			
+		menu.add(btn1);
+		menu.add(btn2);
 		
 		menu.openAtPointer(e); 
   		});
